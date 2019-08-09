8ea30b5b0b8dd27c6aaa1cedd39ff1340aa88cbf
Lu Suelves <luciasuelves@gmail.com>
2019-08-09 13:16:27 +0200

    Ho

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -5,6 +5,7 @@ document.getElementById("profile-photos").onclick = (e) =>
     document.getElementById('botones-perfil').innerHTML += `<a class="nav-link btn" id="planet-button" href="/auth/myPlanet"> Planet</a>`
     document.getElementById('botones-perfil').innerHTML += `<a class="nav-link btn" id="planet-button" href="/auth/myFriends"> Friends</a>`
     document.getElementById('botones-perfil').innerHTML += `<a class="nav-link btn" id="planet-button" href="/auth/myPlans"> Plans</a>`
+    document.getElementById('botones-perfil').innerHTML += `<a class="nav-link btn" id="planet-button" href="/auth/new-post"> New Post</a>`
     }
     else{
     document.getElementById('chaos').classList.add('fila-fotos')
